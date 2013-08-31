03eb9612b7d6d84431d5d3720b9c6627aa51fdd5
praaveen <praaveen@ubuntu.ubuntu-domain>
2013-09-01 00:25:14 +0530

    first test commit

---
+++ gen/com/com/vogella/android/test/simpleactivity/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.com.vogella.android.test.simpleactivity.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
